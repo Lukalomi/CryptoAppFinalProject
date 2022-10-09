70030725177eb76e18d21520b9b6d4c807b6e115
Davit <datotsiskarashvili@gmail.com>
2022-10-09 13:31:02 +0400

    somehow deleted main activity class and added again

--- app/src/main/java/com/example/cryptoappfinalproject/MainActivity.kt
+++ app/src/main/java/com/example/cryptoappfinalproject/MainActivity.kt
@@ -1,18 +1,8 @@
 package com.example.cryptoappfinalproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils.replace
-import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import com.example.cryptoappfinalproject.ui.home.HomeFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
-
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -21,5 +11,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-}
+    }
 }
