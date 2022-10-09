140725c429814b3355827975676a93b9d3402dc7
llomi13 <luka.lomidze00@gmail.com>
2022-10-09 15:02:15 +0400

    Same item cant be added twice in fav list

--- app/src/main/java/com/example/cryptoappfinalproject/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cryptoappfinalproject/ui/home/HomeFragment.kt
@@ -8,6 +8,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageButton
+import androidx.core.content.ContextCompat
 
 
 import androidx.core.view.GravityCompat
@@ -24,6 +26,7 @@ import com.example.cryptoappfinalproject.ui.adapters.CoinsHomeAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import com.example.cryptoappfinalproject.*
+import com.example.cryptoappfinalproject.domain.CryptoCoinsModel
 import com.example.cryptoappfinalproject.ui.favorites.FavoritesViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -50,14 +53,16 @@ class HomeFragment : Fragment() {
         getAllCoinsPager()
         setUpBottomNavigation()
         drawerListener()
+        addCoinsToFavList()
 
-
+    }
+    private fun addCoinsToFavList() {
         lifecycleScope.launch {
             viewModelFav.readAllData().collect {
                 it.forEach {
                     favList.add(it)
                 }
-                Log.d("roommovie", favList.size.toString())
+                Log.d("roomCrypto", favList.size.toString())
 
 
             }
@@ -78,51 +83,96 @@ class HomeFragment : Fragment() {
         adapter = CoinsHomeAdapter(requireContext())
         binding!!.rvHomeCryptoAssets.layoutManager = LinearLayoutManager(activity)
         binding!!.rvHomeCryptoAssets.adapter = adapter
-       favoritesListener()
+        adapter.onFavListener = {
+            favoritesListener(it)
+        }
         binding!!.pbHome.visibility = View.GONE
 
     }
 
 
+    private fun favoritesListener(it: CryptoCoinsModel.CryptoCoinsModelItem) {
+        val crypto = Crypto(
+            uid = 0,
+            image = it.image!!,
+            originalTitle = it.name!!,
+            marketCapRank = it.marketCapRank!!,
+            currentPrice = it.currentPrice!!,
+            priceChangePercentage24h = it.priceChangePercentage24h!!
+        )
 
+        val builder = AlertDialog.Builder(requireContext())
 
-    private fun favoritesListener() {
-        adapter.onFavListener = {
-            val crypto = Crypto(
-                uid = 0,
-                image = it.image!!,
-                originalTitle = it.name!!,
-                marketCapRank = it.marketCapRank!!,
-                currentPrice = it.currentPrice!!,
-                priceChangePercentage24h = it.priceChangePercentage24h!!
-            )
-            val builder = AlertDialog.Builder(requireContext())
+        if(favList.size == 0 ) {
             builder.setPositiveButton("Yes") { _, _ ->
+
                 viewLifecycleOwner.lifecycleScope.launch {
+
                     viewModelFav.insertCrypto(crypto)
                     favList.add(crypto)
-                    Toast.makeText(requireContext()," ${crypto.originalTitle}, Has Been Added To Favorites", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        requireContext(),
+                        " ${crypto.originalTitle}, Has Been Added To Favorites",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
 
                 }
             }
-            builder.setNegativeButton("No") { _, _ -> }
-            builder.setTitle("Add ${crypto.originalTitle}?")
-            builder.setMessage("Are You Sure You Want To Add ${crypto.originalTitle} To Favorite Cryptos?")
-            builder.create().show()
+        }
+        if (favList.toString().contains(crypto.originalTitle)) {
+
+            builder.setPositiveButton("Yes") { _, _ ->
+
+                viewLifecycleOwner.lifecycleScope.launch {
+                    Toast.makeText(
+                        requireContext(),
+                        " ${crypto.originalTitle}, Is Already Added To Favorites",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+            }
 
         }
+        if (!favList.toString().contains(crypto.originalTitle)) {
+
+            builder.setPositiveButton("Yes") { _, _ ->
+                viewLifecycleOwner.lifecycleScope.launch {
+
+                    viewModelFav.insertCrypto(crypto)
+                    favList.add(crypto)
+                    Toast.makeText(
+                        requireContext(),
+                        " ${crypto.originalTitle}, Has Been Added To Favorites",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+
+                }
+            }
+        }
+
+        builder.setNegativeButton("No") { _, _ -> }
+        builder.setTitle("Add ${crypto.originalTitle}?")
+        builder.setMessage("Are You Sure You Want To Add ${crypto.originalTitle} To Favorite Cryptos?")
+        builder.create().show()
+
 
     }
-private fun setUpBottomNavigation() {
-    val navHostFragment = activity!!.supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-    val navController = navHostFragment.navController
-    val bottomNavigationView:BottomNavigationView = activity!!.findViewById(R.id.bottomNavigation)
-    NavigationUI.setupWithNavController(bottomNavigationView,navController)
-}
+
+    private fun setUpBottomNavigation() {
+        val navHostFragment =
+            activity!!.supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigationView: BottomNavigationView =
+            activity!!.findViewById(R.id.bottomNavigation)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+    }
 
 
     private fun drawerListener() {
-        binding?.drawer?.setOnTouchListener(object: Swipe(requireContext()) {
+        binding?.drawer?.setOnTouchListener(object : Swipe(requireContext()) {
 
             override fun onSwipeRight() {
                 startDrawer()
@@ -151,14 +201,11 @@ private fun setUpBottomNavigation() {
     }
 
 
-    
-
-
-
-
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
+        favList.clear()
+        Log.d("roomCrypto", favList.size.toString())
 
     }
 
