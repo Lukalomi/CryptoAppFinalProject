8aca63f4252ff738be09087875c8c4b9da08e3f4
Davit <datotsiskarashvili@gmail.com>
2022-10-15 15:06:42 +0400

    Added firebase Crashlytics

--- app/src/main/java/com/example/cryptoappfinalproject/MainActivity.kt
+++ app/src/main/java/com/example/cryptoappfinalproject/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.cryptoappfinalproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.example.cryptoappfinalproject.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -9,12 +10,14 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private  var binding: ActivityMainBinding? = null
+    private var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
-    }
+
 
     }
+
+}
