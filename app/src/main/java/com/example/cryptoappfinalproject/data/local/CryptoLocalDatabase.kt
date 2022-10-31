b26155c3fb586239c2abc2d571f3752b3f70b2e5
Lukalomi <luka.lomidze00@gmail.com>
2022-10-31 23:46:17 +0400

    Updated the Room schema

--- app/src/main/java/com/example/cryptoappfinalproject/data/local/CryptoLocalDatabase.kt
+++ app/src/main/java/com/example/cryptoappfinalproject/data/local/CryptoLocalDatabase.kt
@@ -7,7 +7,7 @@ import com.example.cryptoappfinalproject.common.BitmapConverter
 
 
 
-@Database(entities = [Crypto::class,Exchanges::class, UserInfo::class], version = 14)
+@Database(entities = [Crypto::class,Exchanges::class, UserInfo::class], version = 15)
 @TypeConverters(BitmapConverter::class)
 abstract class CryptoLocalDatabase: RoomDatabase() {
     abstract fun CryptoDao():CryptoDao
